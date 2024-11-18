05b9b0adcf086def1fe49e80a43ce2d3561515a7
yanghao <yanghao@xgd.com>
2024-11-18 18:24:35 +0800

    修改ui为公有模块

--- src/lib.rs
+++ src/lib.rs
@@ -5,10 +5,12 @@ mod font;
 mod layout;
 mod ui;
 
+pub mod ui;
+
 pub use window::Window;
 pub use widgets::Widget;
 pub use widgets::button::Button;
 pub use widgets::text::Text;
 pub use widgets::container::Container;
 pub use layout::{Rect, Padding, Alignment, Direction};
-pub use ui::{UiLoader, parser::parse_ui};
+// pub use ui::{UiLoader, parser::parse_ui};
